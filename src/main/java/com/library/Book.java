6934f12c8ae42532c6ec68771155f41668bcb7c5
ZZZerock <sviridof12345@yandex.ru>
2025-10-20 19:28:07 +0300

    Add database config and Book entity

---
+++ src/main/java/com/library/Book.java
@@ -0,0 +1,17 @@
+package com.library;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String isbn;
+
+    // конструкторы, геттеры, сеттеры
+}
